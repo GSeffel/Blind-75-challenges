95a59612a02cc9d529ac5c609ac827f32cd6e844
Gustav Seffel <seffel@student.chalmers.se>
2022-11-10 01:25:45 +0100

    test

--- TwoPointers/ThreeSum.java
+++ TwoPointers/ThreeSum.java
@@ -1,5 +1,10 @@
 package Java.Blind75.TwoPointers;
-
+import java.util.*;
 public class ThreeSum {
-    
+    public static List <List <Integer>> threeSum (int [] nums){
+        /*Test thingy to check if git is ok */
+    }
+    public static void main(String[] args) {
+        
+    }
 }
